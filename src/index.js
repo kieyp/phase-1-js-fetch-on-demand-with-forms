3c68d4ba7632ef23e2e412b012373120a0af2014
Boniface <Cheruiyotbony@gmail.com>
2023-11-03 17:54:05 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const init = () => {
-  
-}
+document.addEventListener("DOMContentLoaded",()=>{
 
-document.addEventListener('DOMContentLoaded', init);
+const inputForm=document.querySelector('form');
+inputForm.addEventListener("submit",e=>{
+  e.preventDefault()
+  const input=document.querySelector('input#searchByID')
+  
+
+fetch(`http://localhost:3000/movies/${input.value}`)
+.then(response=>response.json())
+.then(data=>{
+  const title = document.querySelector('section#movieDetails h4');
+      const summary = document.querySelector('section#movieDetails p');
+
+      title.innerText = data.title;
+      summary.innerText = data.summary;
+
+})
+
+})
+
+
+
+})
